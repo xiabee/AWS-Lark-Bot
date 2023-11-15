668258cc43ffd10029c859542ae723144e54a766
xiabee <xiabee@foxmail.com>
2023-11-15 17:06:41 +0800

    update securityhub

--- SecurityHub-Integration/main.go
+++ SecurityHub-Integration/main.go
@@ -12,6 +12,8 @@ import (
 )
 
 func main() {
+	startTime := time.Now() // 记录程序开始时间
+
 	// 加载配置
 	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-west-2"))
 	if err != nil {
@@ -42,10 +44,13 @@ func main() {
 				},
 			},
 		},
+		MaxResults: 100,
 	}
 
-	// 用于统计各个级别告警的数量
+	// 用于统计 CRITICAL 和 HIGH 级别告警的数量
 	countBySeverity := make(map[string]int)
+	// 用于存储告警标题和出现次数
+	titleCount := make(map[string]map[string]int)
 
 	// 循环处理分页
 	for {
@@ -56,17 +61,17 @@ func main() {
 			return
 		}
 
-		// 处理并统计结果
+		// 统计 CRITICAL 和 HIGH 级别的结果
 		for _, finding := range result.Findings {
-			fmt.Println("告警标题:", aws.ToString(finding.Title))
-			fmt.Println("告警描述:", aws.ToString(finding.Description))
-			fmt.Println("最后更新时间:", *finding.UpdatedAt)
-			fmt.Println("告警级别:", finding.Severity.Label)
-			fmt.Println("------------------------------------------------")
-
-			// 更新告警级别计数
 			severityLabel := string(finding.Severity.Label)
-			countBySeverity[severityLabel]++
+			if severityLabel == "CRITICAL" || severityLabel == "HIGH" {
+				countBySeverity[severityLabel]++
+				title := aws.ToString(finding.Title)
+				if titleCount[severityLabel] == nil {
+					titleCount[severityLabel] = make(map[string]int)
+				}
+				titleCount[severityLabel][title]++
+			}
 		}
 
 		// 检查是否还有更多的页面
@@ -78,9 +83,33 @@ func main() {
 		input.NextToken = result.NextToken
 	}
 
-	// 打印告警级别统计
-	fmt.Println("告警级别统计:")
+	// 找出高危和严重告警中出现次数最多的告警
+	var mostFrequentCritical, mostFrequentHigh string
+	var maxCountCritical, maxCountHigh int
+	for title, count := range titleCount["CRITICAL"] {
+		if count > maxCountCritical {
+			maxCountCritical = count
+			mostFrequentCritical = title
+		}
+	}
+	for title, count := range titleCount["HIGH"] {
+		if count > maxCountHigh {
+			maxCountHigh = count
+			mostFrequentHigh = title
+		}
+	}
+
+	// 打印 CRITICAL 和 HIGH 级别告警的数量统计
+	fmt.Println("过去一周内 CRITICAL 和 HIGH 级别告警的数量统计:")
 	for severity, count := range countBySeverity {
 		fmt.Printf("级别 %s: %d 条告警\n", severity, count)
 	}
+
+	fmt.Printf("出现次数最多的 CRITICAL 级别告警: %s, 出现次数: %d\n", mostFrequentCritical, maxCountCritical)
+	fmt.Printf("出现次数最多的 HIGH 级别告警: %s, 出现次数: %d\n", mostFrequentHigh, maxCountHigh)
+
+	// 记录并打印程序运行时间
+	endTime := time.Now() // 记录程序结束时间
+	duration := endTime.Sub(startTime)
+	fmt.Printf("程序运行时间: %s\n", duration)
 }
